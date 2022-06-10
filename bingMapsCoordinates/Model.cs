b351edb62aa228c9b739b66825d75c52fdce971c
jatindersbadwal <jatonder.s.badwal@avanade.com>
2022-06-10 09:37:35 -0400

    latestPhoneCallChecker

---
+++ bingMapsCoordinates/Model.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Point
+    {
+        public string type { get; set; }
+        public IList<double> coordinates { get; set; }
+
+    }
+    public class Address
+    {
+        public string addressLine { get; set; }
+        public string adminDistrict { get; set; }
+        public string adminDistrict2 { get; set; }
+        public string countryRegion { get; set; }
+        public string formattedAddress { get; set; }
+        public string locality { get; set; }
+        public string postalCode { get; set; }
+
+    }
+    public class GeocodePoints
+    {
+        public string type { get; set; }
+        public IList<double> coordinates { get; set; }
+        public string calculationMethod { get; set; }
+        public IList<string> usageTypes { get; set; }
+
+    }
+    public class Resources
+    {
+        public string __type { get; set; }
+        public IList<double> bbox { get; set; }
+        public string name { get; set; }
+        public Point point { get; set; }
+        public Address address { get; set; }
+        public string confidence { get; set; }
+        public string entityType { get; set; }
+        public IList<GeocodePoints> geocodePoints { get; set; }
+        public IList<string> matchCodes { get; set; }
+
+    }
+    public class ResourceSets
+    {
+        public int estimatedTotal { get; set; }
+        public IList<Resources> resources { get; set; }
+
+    }
+    public class Application
+    {
+        public string authenticationResultCode { get; set; }
+        public string brandLogoUri { get; set; }
+        public string copyright { get; set; }
+        public IList<ResourceSets> resourceSets { get; set; }
+        public int statusCode { get; set; }
+        public string statusDescription { get; set; }
+        public string traceId { get; set; }
+
+    }
+}
---
+++ bingMapsCoordinates/bingMapsCoordinates.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using bingMapsModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+//using BingMapsRESTToolkit;
+
+namespace bingMapsCoordinates
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+            Console.ReadLine();
+        }
+        static async Task RunAsync()
+        {
+            Console.WriteLine("\tSEARCH ADDRESS......");
+           // String addressLine = "84 stillman dr";
+            String addressLine = Console.ReadLine();
+            String bingMapsKey = "AsrOHOrxfF61RlwaErE1SLmuf1UktNSyrtMkYQkJNeph25nkbDg6IoMlI3RuKON_";
+            using (var client = new HttpClient())
+            {
+                //go get data
+                client.BaseAddress = new Uri("http://dev.virtualearth.net/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                Console.WriteLine("\tGETTING ADDRESS......");
+                HttpResponseMessage response = await client.GetAsync("REST/v1/Locations?addressLine="+ addressLine + "&maxResults=1 &key="+bingMapsKey);
+                if (response.IsSuccessStatusCode)
+                {                   
+                    var getResponseString = await response.Content.ReadAsStringAsync();
+                    //var getResponse = await response.Content.ReadAsAsync<Address>();
+                    dynamic JSONaddress = JsonConvert.DeserializeObject(getResponseString);
+                    // dynamic data = JSONaddress; 
+                    foreach (var data in JSONaddress.resourceSets)
+                    {
+                        foreach (var data1 in data.resources)
+                        {
+                            foreach (String address in data1.address)
+                            {
+                                Console.WriteLine(address);
+                            };
+                            foreach (String coordinates in data1.geocodePoints[0].coordinates)
+                            {
+                               Console.WriteLine(coordinates);                               
+                            };                    
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
